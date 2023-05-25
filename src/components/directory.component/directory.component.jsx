37711e88390e7c1bb03493d80d6c3222fa69e183
jsky2033 <jsky2033@gmail.com>
2023-05-25 16:04:27 -0400

    directory + category components

---
+++ src/components/directory.component/directory.component.jsx
@@ -0,0 +1,16 @@
+// styles
+import "./directory.styles.scss";
+// component
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
